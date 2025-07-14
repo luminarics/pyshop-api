21680574da7f141f1f3b73ad25d2b80bc49bba99
Denis Połyncew <nubis091@gmail.com>
2025-07-14 20:40:45 +0200

    feat: integrate logging and metrics with Loguru and Prometheus

--- app/main.py
+++ app/main.py
@@ -3,18 +3,28 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.routers import products
 from app.database import init_db
+from prometheus_fastapi_instrumentator import Instrumentator
+from loguru import logger
+
+logger.add("logs/api.log", rotation="1 week", serialize=True)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await init_db()
-    print("DB schema ensured")
+    logger.info("DB schema ensured")
 
     yield
-    print("App shutdown complete")
+    logger.info("App shutdown complete")
 
 
 app = FastAPI(lifespan=lifespan)
+
+instrumentator = Instrumentator()
+
+instrumentator.instrument(app)
+
+instrumentator.expose(app, endpoint="/metrics", include_in_schema=False)
 
 # Routers
 app.include_router(products.router)
