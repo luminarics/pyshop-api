6ea4d7afab1fbd58218285435600c46bca5d7e22
Denis Połyncew <nubis091@gmail.com>
2025-07-16 12:51:03 +0200

    fix: change expiration time calculation to use datetime.utcnow for access token

--- app/auth/authentification.py
+++ app/auth/authentification.py
@@ -48,7 +48,7 @@ async def authenticate_user(
 
 def create_access_token(data: dict) -> str:
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
