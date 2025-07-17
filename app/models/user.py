e2386fff55ca563c221de913fd99b8b16f9f68e0
Denis Połyncew <nubis091@gmail.com>
2025-07-17 13:31:55 +0200

    refactor(user): update User model to use mapped_column for attribute definitions

--- app/models/user.py
+++ app/models/user.py
@@ -2,9 +2,9 @@ from typing import Optional
 from uuid import UUID, uuid4
 from fastapi_users import schemas
 from fastapi_users.db import SQLAlchemyBaseUserTable
-from sqlalchemy import Column
+from sqlalchemy import String, Boolean
 from sqlalchemy.dialects.postgresql import UUID as PostgresUUID
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 
 
 class Base(DeclarativeBase):
@@ -14,15 +14,15 @@ class Base(DeclarativeBase):
 class User(SQLAlchemyBaseUserTable[UUID], Base):
     __tablename__ = "user"
 
-    id = Column(
+    id: Mapped[UUID] = mapped_column(
         PostgresUUID(as_uuid=True), primary_key=True, default=uuid4, nullable=False
     )
-    username: str = Column(str, unique=True, index=True)
-    email: str = Column(str, unique=True, index=True)
-    hashed_password: str = Column(str, nullable=False)
-    is_active: bool = Column(bool, default=True, nullable=False)
-    is_superuser: bool = Column(bool, default=False, nullable=False)
-    is_verified: bool = Column(bool, default=False, nullable=False)
+    username: Mapped[str] = mapped_column(String, unique=True, index=True)
+    email: Mapped[str] = mapped_column(String, unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
 
     class Config:
         arbitrary_types_allowed = True
