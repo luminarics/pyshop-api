84275d4185f82fb704f0fef8a690fceedb60fbda
Denis Połyncew <nubis091@gmail.com>
2025-07-17 13:27:33 +0200

    refactor(user): restructure User model to inherit from DeclarativeBase

--- app/models/user.py
+++ app/models/user.py
@@ -2,12 +2,16 @@ from typing import Optional
 from uuid import UUID, uuid4
 from fastapi_users import schemas
 from fastapi_users.db import SQLAlchemyBaseUserTable
-from sqlmodel import Field, SQLModel
-from pydantic import ConfigDict
+from sqlmodel import Field
+from sqlalchemy.orm import DeclarativeBase
 
 
-class User(SQLModel, SQLAlchemyBaseUserTable[UUID], table=True):  # type: ignore[call-arg]
-    model_config = ConfigDict(arbitrary_types_allowed=True)
+class Base(DeclarativeBase):
+    pass
+
+
+class User(SQLAlchemyBaseUserTable[UUID], Base):
+    __tablename__ = "user"
 
     id: UUID = Field(default_factory=uuid4, primary_key=True)
     username: str = Field(unique=True, index=True)
@@ -17,10 +21,15 @@ class User(SQLModel, SQLAlchemyBaseUserTable[UUID], table=True):  # type: ignore
     is_superuser: bool = Field(default=False, nullable=False)
     is_verified: bool = Field(default=False, nullable=False)
 
+    class Config:
+        arbitrary_types_allowed = True
+
 
 class UserRead(schemas.BaseUser[UUID]):
     username: str
-    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    class Config:
+        arbitrary_types_allowed = True
 
 
 class UserCreate(schemas.BaseUserCreate):
